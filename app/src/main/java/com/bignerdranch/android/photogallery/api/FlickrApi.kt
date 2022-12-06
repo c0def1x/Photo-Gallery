8dfdf0d23794ee20d38351649b9c734c38f06ce8
_codefix <gorbatovofficial@gmail.com>
2022-12-06 14:50:29 +0500

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.photogallery.api
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface FlickrApi
+{
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList&api_key=75c7a8a9f0c153327dbdffff9ba8b904" +
+                "&format=json&nojsoncallback=1&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+
+    @GET
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+}
